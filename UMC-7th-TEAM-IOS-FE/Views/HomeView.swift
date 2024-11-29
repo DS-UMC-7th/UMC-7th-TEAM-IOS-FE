72056b35ed5e138e6ea2506e6372034104ba9a59
kayeon2 <tjsrkdus@gmail.com>
2024-11-30 05:15:44 +0900

    :sparkles: Feat: 검색 레이아웃 구현

--- UMC-7th-TEAM-IOS-FE/Views/HomeView.swift
+++ UMC-7th-TEAM-IOS-FE/Views/HomeView.swift
@@ -11,7 +11,7 @@ import Then
 
 class HomeView: UIView {
     
-    // 네비게이션 바
+    // MARK: - 네비게이션
     private lazy var navigationBar = makeView()
     
     // 로고 이미지
@@ -30,6 +30,26 @@ class HomeView: UIView {
     private let menuButton = UIButton().then {
         $0.setImage(UIImage(systemName: "line.3.horizontal"), for: .normal)
         $0.tintColor = .black
+    }
+    
+    // MARK: - 검색
+    private lazy var searchBar = makeView().then {
+        $0.backgroundColor = UIColor(red: 0.89, green: 0.95, blue: 1.0, alpha: 1.0) // #E3F1FF
+        $0.layer.cornerRadius = 20
+        $0.clipsToBounds = true
+    }
+    
+    // 검색 텍스트 필드
+    private let searchTextField = UITextField().then {
+        $0.placeholder = "책 제목, 작가 이름, 장르 검색"
+        $0.leftView = UIView(frame: CGRect(x: 0.0, y: 0.0, width: 16.0, height: 0.0))
+        $0.leftViewMode = .always
+        $0.font = .systemFont(ofSize: 15)
+    }
+    
+    // 검색 버튼
+    private let searchButton = UIButton().then {
+        $0.setImage(UIImage(named: "icon_search"), for: .normal)
     }
     
     // 뷰 생성 함수
@@ -53,7 +73,8 @@ class HomeView: UIView {
     
     private func setupView() {
         [
-            navigationBar
+            navigationBar,
+            searchBar
         ].forEach {
             addSubview($0)
         }
@@ -65,6 +86,9 @@ class HomeView: UIView {
         ].forEach {
             navigationBar.addSubview($0)
         }
+        
+        searchBar.addSubview(searchTextField)
+        searchBar.addSubview(searchButton)
         
         navigationBar.snp.makeConstraints {
             $0.top.equalTo(safeAreaLayoutGuide)
@@ -88,6 +112,24 @@ class HomeView: UIView {
         menuButton.snp.makeConstraints {
             $0.centerY.equalToSuperview()
             $0.trailing.equalToSuperview().offset(-18)
+            $0.width.height.equalTo(16)
+        }
+        
+        searchBar.snp.makeConstraints {
+            $0.top.equalTo(navigationBar.snp.bottom)
+            $0.leading.equalToSuperview().offset(20)
+            $0.trailing.equalToSuperview().offset(-20)
+            $0.height.equalTo(36)
+        }
+        
+        searchTextField.snp.makeConstraints {
+            $0.top.bottom.leading.equalToSuperview()
+            $0.trailing.equalTo(searchButton.snp.leading).offset(-16)
+        }
+        
+        searchButton.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.trailing.equalToSuperview().offset(-16)
             $0.width.height.equalTo(16)
         }
     }
