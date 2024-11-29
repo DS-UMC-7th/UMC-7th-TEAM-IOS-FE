bad41f1c4258babf26903ff625f649a9bc997da6
김건 <gimgeon@gimgeon-ui-MacBookAir-2.local>
2024-11-30 03:00:09 +0900

    ✨책 상세 페이지 : 책 정보 레이아웃 구현

--- UMC-7th-TEAM-IOS-FE/Views/BookDetailView.swift
+++ UMC-7th-TEAM-IOS-FE/Views/BookDetailView.swift
@@ -60,17 +60,48 @@ class BookDetailView: UIView {
      
     }
     
-    //컴포넌트 추가
+    // 책 정보 배경 뷰
+    private let bookInfoBackgroundView = UIView().then {
+        $0.backgroundColor = UIColor(red: 242/255, green: 249/255, blue: 255/255, alpha: 1)
+        $0.layer.cornerRadius = 12
+    }
+    
+    // 책 정보 타이틀 레이블
+    private let bookInfoTitleLabel = UILabel().then {
+        $0.text = "작가\n출판사\n발행 일자\n쪽수"
+        $0.font = UIFont(name: "Pretendard", size: 15) ?? UIFont.systemFont(ofSize: 15)
+        $0.textColor = UIColor(red: 117/255, green: 148/255, blue: 193/255, alpha: 1)
+        $0.numberOfLines = 0
+        $0.textAlignment = .left
+    }
+    
+    // 책 정보 내용 레이블
+    private let bookInfoContentLabel = UILabel().then {
+        $0.text = "한강\n문학동네\n2021년 09월 09일\n332쪽"
+        $0.font = UIFont(name: "Pretendard", size: 15) ?? UIFont.systemFont(ofSize: 15)
+        $0.textColor = .black
+        $0.numberOfLines = 0
+        $0.textAlignment = .left
+        $0.lineBreakMode = .byCharWrapping
+        $0.numberOfLines = 0
+    }
+    
+    // 컴포넌트 추가
     private func setupSubviews() {
         self.addSubview(blueBackgroundView)
         blueBackgroundView.addSubview(profileImageView)
         blueBackgroundView.addSubview(lightBlueBackgroundView)
         blueBackgroundView.addSubview(bookTitleLabel)
         blueBackgroundView.addSubview(bookDescriptionLabel)
+        
+        self.addSubview(bookInfoBackgroundView)
+        bookInfoBackgroundView.addSubview(bookInfoTitleLabel)
+        bookInfoBackgroundView.addSubview(bookInfoContentLabel)
     }
     
    //레이아웃 설정
     private func setupConstraints() {
+        //프로필 이미지 UI 구성
         blueBackgroundView.snp.makeConstraints {
             $0.width.equalTo(360)
             $0.height.equalTo(380)
@@ -94,7 +125,7 @@ class BookDetailView: UIView {
             $0.centerX.equalTo(lightBlueBackgroundView)
         }
         
-        lightBlueBackgroundView.snp.makeConstraints {
+                lightBlueBackgroundView.snp.makeConstraints {
             $0.width.equalTo(132)
             $0.height.equalTo(14.61)
             $0.top.equalTo(bookTitleLabel.snp.bottom).offset(-13)
@@ -107,6 +138,29 @@ class BookDetailView: UIView {
             $0.top.equalTo(bookTitleLabel.snp.bottom).offset(19)
             $0.left.equalTo(blueBackgroundView).offset(20)
             $0.centerX.equalTo(blueBackgroundView)
+        }
+        
+        //책 정보 레이아웃 구성
+        bookInfoBackgroundView.snp.makeConstraints {
+            $0.width.equalTo(320)
+            $0.height.equalTo(112)
+            $0.top.equalToSuperview().offset(396)
+            $0.left.equalToSuperview().offset(20)
+            $0.centerX.equalTo(blueBackgroundView)
+        }
+        
+        bookInfoTitleLabel.snp.makeConstraints {
+            $0.width.equalTo(56)
+            $0.height.equalTo(88)
+            $0.top.equalTo(bookInfoBackgroundView).offset(12)
+            $0.left.equalTo(bookInfoBackgroundView).offset(12)
+        }
+        
+        bookInfoContentLabel.snp.makeConstraints {
+            $0.width.equalTo(130)
+            $0.height.equalTo(88)
+            $0.top.equalTo(bookInfoBackgroundView).offset(12)
+            $0.left.equalTo(bookInfoTitleLabel.snp.right).offset(20)
         }
     }
 }
